83582296a0536d8e4810b00b0185c3eb1df1832e
shentao <387944558@qq.com>
2016-08-29 14:39:15 +0800

    Initial commit

---
+++ src/main/java/com/example/LifeManagerApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeManagerApplication.class, args);
+	}
+}
